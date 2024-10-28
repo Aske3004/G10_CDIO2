70332d5debc00d73c06dfde1aa40dde9be49ee46
PeterWulff1 <peter@wulff1.dk>
2024-10-28 12:00:59 +0100

    Update Board.java

--- Board.java
+++ Board.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 public class Board{
     // creating a hashmap for the driffert values and a hashmap 
     //for the bankbalance opdates corresponding to the different values.
-    public static boardBalanceUpdate(player, theDiceroll){
+    public static int boardBalanceUpdate(Player player, int theDiceroll){
         HashMap<Integer,Integer> boardbalance = new HashMap<>();
             boardbalance.put(2 , 250);
             boardbalance.put(3 , -100);
@@ -20,19 +20,19 @@ public class Board{
         return player.blance;
 
     }
-    public static fieldstory(theDiceroll){
-        HashMap<Integer,String> fieldstorys = new HashMap<>();
-        boardbalance.put(2 , "You climbed the Tower and found a treasure at the top. You gain 250 gold coins!");
-        boardbalance.put(3 , "You stumbled into a deep Crater and struggled to get out. You lose 100 gold coins.");
-        boardbalance.put(4 , "At the Palace Gates, you’re rewarded by the guards with 100 gold coins for your bravery.");
-        boardbalance.put(5 , "Crossing the Cold Desert drained your resources. You lose 20 gold coins.");
-        boardbalance.put(6 ,  "You enter the Walled City and are hired as a guard, earning 180 gold coins.");
-        boardbalance.put(7 , "You rest peacefully at the Monastery, but gain nothing.");
-        boardbalance.put(8 , "In the Black Cave, shadows steal some of your belongings. You lose 70 gold coins.");
-        boardbalance.put(9 , "You discover a friendly village with Huts in the Mountain and earn 60 gold coins selling goods.");
-        boardbalance.put(10 , "The Werewall appears! You lose 80 gold coins but gain an extra turn as the werewolf howls.");
-        boardbalance.put(11 , -"You fell into a deep pit and need help to get up, you paid 50 ");
-        boardbalance.put(12 , "You found gold in the mauntian and sold it for 650 gold coins!!.... damm you rich");
-        return fieldstory.get(theDiceroll);
+    public static String fieldstory(int theDiceroll){
+        HashMap<Integer,String> fieldStorys = new HashMap<>();
+        fieldStorys.put(2 , "You climbed the Tower and found a treasure at the top. You gain 250 gold coins!");
+        fieldStorys.put(3 , "You stumbled into a deep Crater and struggled to get out. You lose 100 gold coins.");
+        fieldStorys.put(4 , "At the Palace Gates, you’re rewarded by the guards with 100 gold coins for your bravery.");
+        fieldStorys.put(5 , "Crossing the Cold Desert drained your resources. You lose 20 gold coins.");
+        fieldStorys.put(6 ,  "You enter the Walled City and are hired as a guard, earning 180 gold coins.");
+        fieldStorys.put(7 , "You rest peacefully at the Monastery, but gain nothing.");
+        fieldStorys.put(8 , "In the Black Cave, shadows steal some of your belongings. You lose 70 gold coins.");
+        fieldStorys.put(9 , "You discover a friendly village with Huts in the Mountain and earn 60 gold coins selling goods.");
+        fieldStorys.put(10 , "The Werewall appears! You lose 80 gold coins but gain an extra turn as the werewolf howls.");
+        fieldStorys.put(11 , -"You fell into a deep pit and need help to get up, you paid 50 ");
+        fieldStorys.put(12 , "You found gold in the mauntian and sold it for 650 gold coins!!.... damm you rich");
+        return fieldStory.get(theDiceroll);
     }
 }
