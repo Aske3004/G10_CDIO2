2ea388aed57bc466540abe3e8d22b7dcd9419dc4
Aske3004 <89708841+Aske3004@users.noreply.github.com>
2024-10-29 13:10:49 +0100

    Update Player.java

--- G10_CDIO2/Player.java
+++ G10_CDIO2/Player.java
@@ -1,7 +1,9 @@
 public class Player {
     String playerName;
+    Balance playerBalance;
 
     public Player(String pName){
         this.playerName = pName;
+        playerBalance = new Balance(pName);
     }
 }
