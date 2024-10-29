a2b10bfcdcd91815b0efc5fcd917c092afa84233
Aske3004 <89708841+Aske3004@users.noreply.github.com>
2024-10-29 13:11:59 +0100

    Update Balance.java

--- G10_CDIO2/Balance.java
+++ G10_CDIO2/Balance.java
@@ -7,23 +7,28 @@ public class Balance {
         this.balance = 1000;
     }
 
-    public void addToBalance(int amount){
+    public Boolean addToBalance(int amount){
         if (amount > 0){
             balance += amount;
+            return true;
         } else {
             System.out.println("Error! Amount can not be negative.");
+            return false;
         }
     }
 
-    public void withdrawFromBalance(int amount){
+    public Boolean withdrawFromBalance(int amount){
         if (amount > 0){
             if (balance - amount >= 0){
                 balance -= amount;
+                return true;
             } else {
                 System.out.println("Error! Not enough money in account.");
+                return false;
             }
         } else {
             System.out.println("Error! Amount can not be negative.");
+            return false;
         }
     }
 
